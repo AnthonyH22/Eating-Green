6522e53d5f04df345e27c526b7108618dc88c6a9
AnthonyH22 <121174924+AnthonyH22@users.noreply.github.com>
2023-04-30 18:36:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+// declaring and initializing the sections for each page
 const homeSection = document.querySelector('#home');
 const aboutSection = document.querySelector('#about');
 const menuSection = document.querySelector('#menu');
 const contactSection = document.querySelector('#contact');
 
+// declaring and initializing the links for each page
 const homeLink = document.querySelector('a[href="#home"]');
 const aboutLink = document.querySelector('a[href="#about"]');
 const menuLink = document.querySelector('a[href="#menu"]');
 const contactLink = document.querySelector('a[href="#contact"]');
 
+// if 'home' is clicked on, it will only display the home section and keep everything else hidden.
 homeLink.addEventListener('click', () => {
   homeSection.style.display = 'block';
   menuSection.style.display = 'none';
@@ -15,6 +18,7 @@ homeLink.addEventListener('click', () => {
   contactSection.style.display = 'none';
 });
 
+// if 'about' is clicked on, it will only display the home section and keep everything else hidden.
 aboutLink.addEventListener('click', () => {
   homeSection.style.display = 'none';
   menuSection.style.display = 'none';
@@ -22,6 +26,7 @@ aboutLink.addEventListener('click', () => {
   contactSection.style.display = 'none';
 });
 
+// if 'menu'' is clicked on, it will only display the home section and keep everything else hidden.
 menuLink.addEventListener('click', () => {
   homeSection.style.display = 'none';
   menuSection.style.display = 'block';
@@ -29,6 +34,7 @@ menuLink.addEventListener('click', () => {
   contactSection.style.display = 'none';
 });
 
+// if 'contact' is clicked on, it will only display the home section and keep everything else hidden.
 contactLink.addEventListener('click', () => {
   homeSection.style.display = 'none';
   menuSection.style.display = 'none';
